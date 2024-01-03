ff660f35658af7080dce29241d4b2307f739c028
Rita Tang <doggymint@gmail.com>
2024-01-03 17:35:35 -0500

    Add Input component for slider input

---
+++ src/components/Input.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  props: {
+    input: { type: String, label: String, value: Number, unit: String },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <span
+      >{{ input.label }}:
+      <strong>{{
+        input.unit === "$" ? input.unit + input.value : input.value + input.unit
+      }}</strong></span
+    >
+    <input :type="input.type" v-model="input.value" />
+  </div>
+</template>
+
+<style scoped></style>
