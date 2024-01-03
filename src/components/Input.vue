3c8c4970a617ad08760d5a341a1c7fe22878117a
Rita Tang <doggymint@gmail.com>
2024-01-03 17:42:25 -0500

    Add maxValue method to determine the max value dynamically

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -3,6 +3,18 @@ export default {
   props: {
     input: { type: String, label: String, value: Number, unit: String },
   },
+  methods: {
+    maxValue() {
+      switch (this.input.unit) {
+        case "$":
+          return 1000000;
+        case "%":
+          return 10;
+        case " years":
+          return 30;
+      }
+    },
+  },
 };
 </script>
 
@@ -14,7 +26,7 @@ export default {
         input.unit === "$" ? input.unit + input.value : input.value + input.unit
       }}</strong></span
     >
-    <input :type="input.type" v-model="input.value" />
+    <input :type="input.type" v-model="input.value" :max="maxValue()"/>
   </div>
 </template>
 
