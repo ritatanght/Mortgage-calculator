7e0ebe3c1d794ab35f6463a9339b3e4c86ac8dea
Rita Tang <doggymint@gmail.com>
2024-01-03 21:26:51 -0500

    Add Display component for showing results from calculation

---
+++ src/components/Display.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    label: String,
+    value: Number,
+    //{ label: String, value: Number },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <p>{{ label }}</p>
+    <p>${{ value % 1 === 0 ? value : value.toFixed(2) }}</p>
+  </div>
+</template>
+
+<style scoped></style>
